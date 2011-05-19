7fe48e6791486936b6b8c35238c11c9fd2fc752b
jedi95 <jedi95@16499f8b-67fa-4e96-9604-7923d1e94fee>
2011-05-19 22:23:47 +0000

    Added debug fix for idling miner problem.
    git-svn-id: http://svn3.xp-dev.com/svn/phoenix-miner/trunk@98 16499f8b-67fa-4e96-9604-7923d1e94fee

--- minerutil/client3420.py
+++ minerutil/client3420.py
@@ -636,7 +636,8 @@ class Agent(object):
     @since: 9.0
     """
     _protocol = HTTP11ClientProtocol
-    maxConnections = 2 # RFC 2616: A single-user client SHOULD NOT
+    #this should be 2, but for debug i'm going to remove this limit for now
+    maxConnections = 100 # RFC 2616: A single-user client SHOULD NOT
                        # maintain more than 2 connections with any
                        # server or proxy.
 
